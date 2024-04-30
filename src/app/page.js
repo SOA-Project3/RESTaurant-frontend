9b3e8d481024756b6a866a77059655fcf726f6f5
LuisPeMoraRod <luismorarod98@estudiantec.cr>
2024-04-29 22:01:30 -0600

    doc: update README.md

--- src/app/page.js
+++ src/app/page.js
@@ -4,27 +4,27 @@ import { useEffect, useState } from "react";
 import { getMenu } from "@/services";
 import Menu from "@/components/menu/Menu";
 const Home = () => {
-  const { menu, setMenu } = useAppContext();
-  const [meals, setMeals] = useState(null);
+  const { menu, setMenu } = useAppContext(); // react context to handle menu as an app-wide variable
+  const [meals, setMeals] = useState(null); // state that handles meals in screen
 
   useEffect(() => {
     const fetchMenu = async () => {
       if (!menu) {
         try {
-          const data = await getMenu();
-          setMenu(data.menu);
-          setMeals(data.menu.meal);
+          const data = await getMenu(); // use service that fetches menu data
+          setMenu(data.menu); // set meals in context
+          setMeals(data.menu.meal); // set meals in local state
         } catch (error) {
           console.error("Menu fetch error:", error.message);
         }
       }
     };
 
-    if (!menu) fetchMenu();
-    else setMeals(menu.meal);
-  }, [menu, setMenu]); // Only re-run the effect if menu or setMenu changes
+    if (!menu) fetchMenu(); // if data hasn't been fetched
+    else setMeals(menu.meal); // if data is already available, render meals
+  }, [menu, setMenu]);
 
-  if (!!meals) return <Menu menu={meals} />;
+  if (!!meals) return <Menu menu={meals} />; // render Menu
 };
 
 export default Home;
