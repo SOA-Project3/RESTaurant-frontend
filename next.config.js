de5f406e8a1af7cac425d8c2ace5e33510ab6dfb
LuisPeMoraRod <luismorarod98@estudiantec.cr>
2024-05-14 11:45:59 -0600

    fix: remove next.config.js unnecessary properties

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
-const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: isProd
-    ? "https://us-central1-soa-g6-p2.cloudfunctions.net/nextjs"
-    : undefined,
-  env: {
-    basePath: isProd ? "/nextjs" : "",
-  },
 };
